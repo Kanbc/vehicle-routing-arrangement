2d6177b6f4dbfc4651ba0149f137ef086397598a
Pawarisson <kan.pawarisson@gmail.com>
2019-02-11 15:37:29 +0700

    change demo data and .gitignore

--- resource/matlab/runFile.m
+++ resource/matlab/runFile.m
@@ -6,20 +6,20 @@ addpath('function/');
 
 % ==== Input Variable ====
 % -- real data
-% job_done = {'K'}; % start_point
-% node_names = readtable('data/node_names.csv'); % node_names
-% distances = csvread('data/distances_matrix.csv'); % distances
-% tasks_with_nodes = readtable('data/tasks.csv');
-% tasks = tasks_with_nodes(:,{'Start','End','Count'});
-% tasks.Path = strcat(tasks.Start, '-', tasks.End); % tasks
-
-% -- demo data
-job_done = {'A'}; % start_point
-node_names = readtable('data/demo/node_names.csv'); % node_names
-distances = csvread('data/demo/distances_matrix.csv'); % distances
-tasks_with_nodes = readtable('data/demo/tasks.csv');
+job_done = {'S'}; % start_point
+node_names = readtable('data/node_names.csv'); % node_names
+distances = csvread('data/distances_matrix.csv'); % distances
+tasks_with_nodes = readtable('data/tasks.csv');
 tasks = tasks_with_nodes(:,{'Start','End','Count'});
 tasks.Path = strcat(tasks.Start, '-', tasks.End); % tasks
+
+% -- demo data
+% job_done = {'A'}; % start_point
+% node_names = readtable('data/demo/node_names.csv'); % node_names
+% distances = csvread('data/demo/distances_matrix.csv'); % distances
+% tasks_with_nodes = readtable('data/demo/tasks.csv');
+% tasks = tasks_with_nodes(:,{'Start','End','Count'});
+% tasks.Path = strcat(tasks.Start, '-', tasks.End); % tasks
 
 % ==== Output ====
 tic
